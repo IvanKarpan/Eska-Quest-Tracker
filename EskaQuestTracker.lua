e15e8d046d59126705e061989f17c5e236533631
Skamer <Skamer@users.noreply.github.com>
2017-10-02 00:33:45 +0200

    Tagged as release

--- EskaQuestTracker.lua
+++ EskaQuestTracker.lua
@@ -3,7 +3,7 @@
 -- @Author   : Skamer <https://mods.curse.com/members/DevSkamer>              --
 -- @Website  : https://wow.curseforge.com/projects/eska-quest-tracker         --
 -- ========================================================================== --
-Scorpio                   "EskaQuestTracker"                             "1.4.2"
+Scorpio                   "EskaQuestTracker"                             "1.4.3"
 -- ========================================================================== --
 import "EQT"
 import "System.Collections"
