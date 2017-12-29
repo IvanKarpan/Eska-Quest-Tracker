cfe71cf567545a88e82ab1b67dfbf083108702bf
Skamer <Skamer@users.noreply.github.com>
2017-12-29 05:34:49 +0100

    The quest categories can now be disabled.

--- Classes/QuestBlock.lua
+++ Classes/QuestBlock.lua
@@ -15,14 +15,36 @@ class "QuestBlock" inherit "Block"
   __Arguments__{ Quest }
   function AddQuest(self, quest)
     if not self.quests:Contains(quest) then
-      local header = self:GetHeader(quest.header)
-      if not header then
-        header = self:NewHeader(quest.header)
+      if Options:Get("quest-categories-enabled") then
+        local header = self:GetHeader(quest.header)
+        if not header then
+          header = self:NewHeader(quest.header)
+        end
+        header:AddQuest(quest)
+      else
+        quest:SetParent(self.frame)
+        quest.OnHeightChanged = function(_, new, old)
+          self.height = self.height + (new - old)
+        end
+        quest.OnDistanceChanged = function() self.OnDrawRequest() end
       end
-      header:AddQuest(quest)
       self.quests:Insert(quest)
       self.OnDrawRequest()
       _M:FireSystemEvent("EQT_QUESTBLOCK_QUEST_ADDED", quest)
+    end
+  end
+
+  __Arguments__ { Quest }
+  function RemoveQuestFromHeader(self, quest)
+    local header = self:GetHeader(quest.header)
+    if header then
+      header:RemoveQuest(quest)
+      if header:GetQuestNum() == 0 then
+        header.OnHeightChanged = nil
+        header.OnDrawRequest = nil
+        self:RemoveHeader(quest.header)
+        header.isReusable = true
+      end
     end
   end
 
@@ -34,21 +56,15 @@ class "QuestBlock" inherit "Block"
     end
   end
 
-  __Arguments__{ Quest }
+  __Arguments__ { Quest }
   function RemoveQuest(self, quest)
     self.quests:Remove(quest)
-    local header = self:GetHeader(quest.header)
-    if header then
-      header:RemoveQuest(quest)
-      if header:GetQuestNum() == 0 then
-        header.OnHeightChanged = nil
-        header.OnDrawRequest = nil
 
-        self:RemoveHeader(quest.header)
-        header.isReusable = true
-        self.OnDrawRequest()
-      end
+    if Options:Get("quest-categories-enabled") then
+      self:RemoveQuestFromHeader(quest)
     end
+    self.OnDrawRequest()
+
     _M:FireSystemEvent("EQT_QUESTBLOCK_QUEST_REMOVED", quest)
     quest.isReusable = true
   end
@@ -92,49 +108,141 @@ class "QuestBlock" inherit "Block"
     self.headers[name] = nil
   end
 
+  -- This function is called when the frame needs to update the anchors of its children.
+  -- You should avoid to call it directly, except if you know you're doing.
+  -- It's prefered to use self.OnDrawRequest instead, that is a safe way to call indirectly Draw().
   __Arguments__()
   function Draw(self)
+    local enableCategories = Options:Get("quest-categories-enabled")
 
     local previousFrame
     local height = 0
 
-    -- Header compare function
-    local function HeaderSortMethod(a, b)
-      if a.nearestQuestDistance ~= b.nearestQuestDistance then
-        return a.nearestQuestDistance < b.nearestQuestDistance
+    if enableCategories then
+      -- Header compare function
+      local function HeaderSortMethod(a, b)
+        if a.nearestQuestDistance ~= b.nearestQuestDistance then
+          return a.nearestQuestDistance < b.nearestQuestDistance
+        end
+        return a.name < b.name
       end
-      return a.name < b.name
-    end
 
-    local mustBeAnchored = false
-    for index, header in self.headers.Values:ToList():Sort(HeaderSortMethod):GetIterator() do
-
-        if (not header._sortIndex) or (header._sortIndex ~= index) then
-          mustBeAnchored = true
+      for index, header in self.headers.Values:ToList():Sort(HeaderSortMethod):GetIterator() do
+        if not header:IsShown() then
+          header:Show()
         end
 
-        if mustBeAnchored then
-          if not header:IsShown() then
-            header:Show()
-          end
+        header:ClearAllPoints()
 
-          if index == 1 then
-            header.frame:SetPoint("TOPLEFT", 0, -35)
-            header.frame:SetPoint("TOPRIGHT", 0, -35)
-            isFirst = false
-          else
-            header.frame:SetPoint("TOPLEFT", previousFrame, "BOTTOMLEFT", 0, -2)
-            header.frame:SetPoint("TOPRIGHT", previousFrame, "BOTTOMRIGHT")
-          end
+        if index == 1 then
+          header.frame:SetPoint("TOPLEFT", 0, -35)
+          header.frame:SetPoint("TOPRIGHT", 0, -35)
+        else
+          header.frame:SetPoint("TOPLEFT", previousFrame, "BOTTOMLEFT", 0, -2)
+          header.frame:SetPoint("RIGHT", previousFrame, "BOTTOMRIGHT")
         end
-          header._sortIndex = index
 
-          height = height + header.height
-          previousFrame = header.frame
+        previousFrame = header.frame
+      end
+    else
+      -- Quest compare function (Priorty : Distance > ID > Name)
+      local function QuestSortMethod(a, b)
+        if a.distance ~= b.distance then
+          return a.distance < b.distance
+        end
 
+        if a.id ~= b.id then
+          return a.id < b.id
+        end
+        return a.name < b.name
+      end
+
+      for index, quest in self.quests:Sort(QuestSortMethod):GetIterator() do
+        if not quest:IsShown() then
+          quest:Show()
+        end
+
+        if index == 1 then
+          quest.frame:SetPoint("TOPLEFT", self.frame, "TOPLEFT", 0, -36) -- -36
+          quest.frame:SetPoint("TOPRIGHT", self.frame, "TOPRIGHT")
+        else
+          quest.frame:SetPoint("TOPLEFT", previousFrame, "BOTTOMLEFT", 0, -10)
+          quest.frame:SetPoint("TOPRIGHT", previousFrame, "BOTTOMRIGHT")
+        end
+
+        previousFrame = quest.frame
+
+      end
     end
-    self.height = self.baseHeight + height + 10
+
+    self:CalculateHeight()
   end
+
+  function CalculateHeight(self)
+    local height = self.baseHeight
+    if Options:Get("quest-categories-enabled") then
+      for index, header in self.headers.Values:ToList():GetIterator() do
+        local offset = 2
+
+        height = height + header.height + offset
+      end
+    else
+      for index, quest in self.quests:GetIterator() do
+        local offset = 10
+
+        height = height + quest.height + offset
+      end
+    end
+
+    self.height = height
+  end
+
+  function EnableCategories(self)
+    for index, quest in self.quests:GetIterator() do
+      local header = self:GetHeader(quest.header)
+
+      -- Remove event register by the block
+      quest.OnHeightChanged = nil
+      quest.OnDistanceChanged = nil
+
+      if not header then
+        header = self:NewHeader(quest.header)
+      end
+      -- The quest header handles everything (anchor, register event, ...)
+      header:AddQuest(quest)
+    end
+    -- Request a draw to displaying changes
+    self.OnDrawRequest()
+  end
+
+  function DisableCategories(self)
+    for index, quest in self.quests:GetIterator() do
+      self:RemoveQuestFromHeader(quest)
+
+      -- Don't forget to change the parent
+      quest:SetParent(self.frame)
+
+      -- Register events
+      quest.OnHeightChanged = function(_, new, old)
+        self.height = self.height + (new - old )
+      end
+      quest.OnDistanceChanged = function() self.OnDrawRequest() end
+    end
+    -- Request a draw to displaying changes
+    self.OnDrawRequest()
+  end
+
+
+  __Static__() function SetCategoriesEnabled(enabled)
+    for obj in pairs(_QuestBlockCache) do
+      if enabled then
+        obj:EnableCategories()
+      else
+        obj:DisableCategories()
+      end
+    end
+  end
+
 
   __Arguments__ { Argument(Theme.SkinFlags, true, ALL) }
   __Static__() function RefreshAll(skinFlags)
@@ -170,6 +278,10 @@ class "QuestBlock" inherit "Block"
   end
 endclass "QuestBlock"
 
+-- /run EQT.Options:Set("quest-categories-enabled", true)
 function OnLoad(self)
+  Options:Register("quest-categories-enabled", true, "quests/setCategoriesEnabled")
+
   CallbackHandlers:Register("quests/refresher", CallbackHandler(QuestBlock.RefreshAll))
+  CallbackHandlers:Register("quests/setCategoriesEnabled", CallbackHandler(QuestBlock.SetCategoriesEnabled))
 end
